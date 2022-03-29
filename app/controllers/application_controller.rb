a4dcc4e9be8051b5de5b1130162791129b9b0f01
Julio Aguilera Jr <julioaguilera337@gmail.com>
2022-03-28 21:05:27 -0500

    commit tree bec544b832fc9b720295ce4a8dec4ee034d06dd5

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,8 +17,13 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/signup" do
+    if params[:username] == "" || params[:password] == ""
+      redirect "/failure"
+    else
+      User.create(username: params[:username], password: params[:password])
+      redirect "/login"
+    end
     #your code here
-
   end
 
   get '/account' do
@@ -32,6 +37,13 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
+    @user = User.find_by(username: params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect "/account"
+    else
+      redirect "/failure"
+    end
     ##your code here
   end
 
